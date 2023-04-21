f8c4b8dd00fac1d77ed287e6ba0b0984e5913947
Wei Gao <wegao@suse.com>
2023-04-21 09:50:15 -0400

    d

--- tests/migration/online_migration/zypper_migration.pm
+++ tests/migration/online_migration/zypper_migration.pm
@@ -158,21 +158,26 @@ sub run {
     # wait long time for snapper to settle down
     assert_screen 'root-console', 600;
 
-    # We can't use 'keepconsole' here, because sometimes a display-manager upgrade can lead to a screen change
-    # during restart of the X/GDM stack
-    if (is_sle_micro) {
-        check_reboot_changes;
-        check_migrated_version;
-    } else {
-        power_action('reboot', textmode => 1);
-        reconnect_mgmt_console if is_pvm;
-        # Do not attempt to log into the desktop of a system installed with SLES4SAP
-        # being prepared for upgrade, as it does not have an unprivileged user to test
-        # with other than the SAP Administrator
-        #
-        # sometimes reboot takes longer time after online migration, give more time
-        $self->wait_boot(textmode => !is_desktop_installed, bootloader_time => 500, ready_time => 600, nologin => is_sles4sap);
-    }
+    #wegao first upload y2log
+    assert_script_run 'save_y2logs /tmp/y2logs_clone.tar.bz2';
+    upload_logs '/tmp/y2logs_clone.tar.bz2';
+
+    my $waittime = 200 * get_var('TIMEOUT_SCALE', 1);
+
+    # 'snapper list' will cost a lots of time to finish if btrfs-balance.sh was triggered
+    # To make it faster, we'd check if snapper have disable-used-space option.
+    # This is a workaround for bsc#1167353.
+    record_info('workaround for bsc#1167353');
+    script_run("(snapper --help | grep -q -- --disable-used-space && snapper list --disable-used-space || snapper list) | tee /dev/$serialdev", 0);
+    # Check if the snapshot called 'before update' is there
+    wait_serial('pre\s*(\|[^|]*){4,}\s*\|\s*number\s*\|\s*before (update|online migration)\s*\|\s*important=yes', $waittime)
+      || die 'upgrade snapshots test failed';
+
+    script_run("(snapper --help | grep -q -- --disable-used-space && snapper list --disable-used-space || snapper list) | tee /dev/$serialdev", 0);
+    # Check if the snapshot called 'after update' is there
+    wait_serial('post\s*(\|[^|]*){4,}\s*\|\s*number\s*\|\s*after (update|online migration)\s*\|\s*important=yes', $waittime)
+      || die 'upgrade snapshots test failed';
+
 }
 
 sub post_fail_hook {
