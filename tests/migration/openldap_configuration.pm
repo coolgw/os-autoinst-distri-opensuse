ff3551e327ad96f172d2111fb517b990ef60ef3e
Wei Gao <wegao@suse.com>
2023-04-25 03:15:46 -0400

    d

--- tests/migration/openldap_configuration.pm
+++ tests/migration/openldap_configuration.pm
@@ -20,12 +20,14 @@ sub run {
     my ($self) = @_;
     select_serial_terminal;
 
+=begin
     # Install openldap since we need use slaptest tools
     zypper_call("in sssd sssd-tools sssd-ldap openldap2 openldap2-client");
-
+=cut
     # Disable and stop the nscd daemon because it conflicts with sssd
     disable_and_stop_service("nscd");
 
+=begin
     # On newer environments, nsswitch.conf is located in /usr/etc
     # Copy it to /etc directory
     script_run 'f=/etc/nsswitch.conf; [ ! -f $f ] && cp /usr$f $f';
@@ -33,11 +35,9 @@ sub run {
     assert_script_run "sed -i 's/^passwd:.*/passwd: compat sss/' /etc/nsswitch.conf";
     assert_script_run "sed -i 's/^group:.*/group: compat sss/' /etc/nsswitch.conf";
     assert_script_run "cat /etc/nsswitch.conf";
-
     # Prepare test env
     assert_script_run "cd; curl -L -v " . autoinst_url . "/data/openldap_to_389ds > openldap_to_389ds.data && cpio -id < openldap_to_389ds.data && mv data test && ls test";
     assert_script_run("cd test");
-
     # We need start openldap to kick out date base file which stored in directory
     assert_script_run "mkdir /tmp/ldap-sssdtest";
     if (is_tumbleweed) {
@@ -49,21 +49,19 @@ sub run {
     assert_script_run "cat ./slapd.conf";
     my $slapd_command = (is_sle('<=12-sp5')) ? "/usr/lib/openldap/slapd" : "slapd";
     assert_script_run "$slapd_command -h 'ldap:///' -f slapd.conf";
-
     assert_script_run "ldapadd -x -D 'cn=root,dc=ldapdom,dc=net' -wpass -f db.ldif";
     assert_script_run "killall slapd";
     assert_script_run "ps -aux | grep slapd";
-
     # setup sssd
     assert_script_run "cp ./sssd.conf /etc/sssd/sssd.conf";
     systemctl("stop sssd");
     assert_script_run "rm -rf /var/lib/sss/db/*";
     systemctl("restart sssd");
     systemctl("status sssd");
-
     # Prepare data file for migration
     assert_script_run "mkdir slapd.d";
     assert_script_run "slaptest -f slapd.conf -F ./slapd.d";
+=cut
 
 }
 
@@ -75,3 +73,4 @@ sub post_fail_hook {
 }
 
 1;
+
