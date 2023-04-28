5e407ec3dbe7735d0b2f10547fd9bfdd31dc6218
Wei Gao <wegao@suse.com>
2023-04-27 23:15:15 -0400

    debug

--- tests/yam/agama/auto.pm
+++ tests/yam/agama/auto.pm
@@ -9,14 +9,26 @@ use strict;
 use warnings;
 
 use testapi;
+use serial_terminal; 
 
 
 sub run {
     assert_screen('agama-main-page', 120);
     assert_screen('agama-installing', 60);
 
+    $testapi::password = 'linux';
+    select_serial_terminal;
+    script_run('journalctl -u agama-auto');
+
     my @tags = ("welcome-to", "login");
     assert_screen \@tags, 960;
+}
+
+
+sub post_fail_hook {
+	$testapi::password = 'linux';
+	select_serial_terminal;
+	script_run('journalctl -u agama-auto');
 }
 
 1;
