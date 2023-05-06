64b3daae9d0227efaaac532ca1abb8a55128396d
Wei Gao <wegao@suse.com>
2023-05-06 02:35:26 -0400

    d

--- data/yam/agama/auto/alp-tw.sh
+++ data/yam/agama/auto/alp-tw.sh
@@ -1,8 +1,28 @@
 set -ex
 
-/usr/bin/agama config set software.product=Tumbleweed
-/usr/bin/agama config set user.userName=joe user.password=doe
-/usr/bin/agama config set root.password=nots3cr3t
+tmpdir=$(mktemp --directory --suffix "-agama")
+echo "working on $tmpdir"
+
 /usr/bin/sleep 30
+
+cat << EOF > ${tmpdir}/profile.json
+{
+   "software": {
+      "product": "Tumbleweed"
+   },
+   "user": {
+      "fullName": "Jane Doe",
+      "password": "doe",
+      "userName": "joe"
+   },
+   "root": {
+      "password": "nots3cr3t"
+   }
+}
+EOF
+
+/usr/bin/agama profile validate "${tmpdir}/profile.json" || echo "Validation failed"
+/usr/bin/agama config load "${tmpdir}/profile.json"
+
 /usr/bin/agama install
 /sbin/reboot
